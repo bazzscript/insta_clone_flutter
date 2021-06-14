989e01851046fdba3a1a8dc9ffdd1a17079c0eaf
emexbazz <bmexit@gmail.com>
2021-06-14 04:51:16 +0100

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,128 @@
+import 'package:flutter/material.dart';
+import './home.dart';
+import './search.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of  this application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Instagram Flutter Clone',
+      theme: ThemeData(
+        iconTheme: IconThemeData(color: Color.fromRGBO(40, 40, 40, 1)),
+        appBarTheme: AppBarTheme(
+          elevation: 1,
+          color: Colors.white,
+          iconTheme: IconThemeData(color: Color.fromRGBO(40, 40, 40, 1)),
+        ),
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int currentPage = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: currentPage == 1 ? SearchPage() : HomePage(),
+      bottomNavigationBar: BottomAppBar(
+        // for the navigation bar in the bottom bar
+        child: Row(
+          children: [
+            // The Home Button
+            Spacer(),
+            IconButton(
+              onPressed: () {
+                setState(() {
+                  currentPage = 0;
+                });
+              },
+              icon: Icon(
+                Icons.home,
+                color: currentPage == 0
+                    ? Color.fromRGBO(203, 73, 101, 1)
+                    : Color.fromRGBO(40, 40, 40, 1),
+              ),
+            ),
+            // The Search & Explore Button
+            Spacer(),
+            IconButton(
+              onPressed: () {
+                setState(() {
+                  currentPage = 1;
+                });
+              },
+              icon: Icon(
+                Icons.search,
+                color: currentPage == 1
+                    ? Color.fromRGBO(203, 73, 101, 1)
+                    : Color.fromRGBO(40, 40, 40, 1),
+              ),
+            ),
+            // Tne Reels Button
+            Spacer(),
+            IconButton(
+              onPressed: () {
+                setState(() {
+                  currentPage = 2;
+                });
+              },
+              icon: Icon(
+                Icons.ondemand_video,
+                color: currentPage == 2
+                    ? Color.fromRGBO(203, 73, 101, 1)
+                    : Color.fromRGBO(40, 40, 40, 1),
+              ),
+            ),
+            // The Fourth Button
+            Spacer(),
+            IconButton(
+              onPressed: () {
+                setState(() {
+                  currentPage = 3;
+                });
+              },
+              icon: Icon(
+                Icons.card_travel,
+                color: currentPage == 3
+                    ? Color.fromRGBO(203, 73, 101, 1)
+                    : Color.fromRGBO(40, 40, 40, 1),
+              ),
+            ),
+            // The Profile Button
+            Spacer(),
+            IconButton(
+              onPressed: () {
+                setState(() {
+                  currentPage = 4;
+                });
+              },
+              icon: Icon(
+                Icons.person,
+                color: currentPage == 4
+                    ? Color.fromRGBO(203, 73, 101, 1)
+                    : Color.fromRGBO(40, 40, 40, 1),
+              ),
+            ),
+            Spacer(),
+          ],
+        ),
+      ),
+    );
+  }
+}
